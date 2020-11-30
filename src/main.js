3f5d05550bd9afddcbb1e5fe0233f19ac6aa0666
flaviodiminuto <flavio.diminuto@outlook.com>
2020-11-30 01:54:36 -0300

    [limpeza] Removendo bootstrap do projeto

--- src/main.js
+++ src/main.js
@@ -4,16 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import SuiVue from 'semantic-ui-vue'
-// import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'semantic-ui-css/semantic.min.css'
 
 Vue.config.productionTip = false
 Vue.use(SuiVue)
-// Vue.use(BootstrapVue)
-// Vue.use(IconsPlugin)
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
