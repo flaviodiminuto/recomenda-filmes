6257fd648d89adec288d946f7847c505543b010f
flaviodiminuto <flavio.diminuto@outlook.com>
2020-12-05 02:10:41 -0300

    [update] Recomendar filmes

--- src/main.js
+++ src/main.js
@@ -6,12 +6,10 @@ import router from './router'
 import VueRouter from 'vue-router'
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css'
-import VueCookies from 'vue-cookies'
 
 Vue.config.productionTip = false
 Vue.use(SuiVue)
 Vue.use(VueRouter)
-Vue.use(VueCookies)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
